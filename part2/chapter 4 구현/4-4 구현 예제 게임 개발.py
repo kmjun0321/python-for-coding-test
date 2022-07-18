0cc13e76c90d2a0e3849a6925df725031d497847
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-07-18 22:07:18 +0900

    Update 4-4 구현 예제 게임 개발.py

--- part2/chapter 4 구현/4-4 구현 예제 게임 개발.py
+++ part2/chapter 4 구현/4-4 구현 예제 게임 개발.py
@@ -1,16 +1,18 @@
+# 4-4 구현 예제 게임 개발
+
 n, m = map(int, input().split())
 a, b, d = map(int, input().split())
 
-map = []
+input_map = []
 
 for i in range(n):
-  map.append(list(map(int, input().split())))
+  input_map.append(list(map(int, input().split())))
 
 move_steps = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 result = 1
-
-move_steps[a][b] = 2
+count = 0
+input_map[a][b] = 2
 
 while True:
   d = d - 1
@@ -18,11 +20,11 @@ while True:
     d = 3
   next_a = a + move_steps[d][0]
   next_b = b + move_steps[d][1]
-  if map[next_a][next_b] == 0:
+  if input_map[next_a][next_b] == 0:
     a = next_a
     b = next_b
     result += 1
-    max[a][b] = 2
+    input_map[a][b] = 2
     count = 0
 
   else:
@@ -30,7 +32,7 @@ while True:
     if count == 4:
       next_a = a + move_steps[d-2][0]
       next_b = b + move_steps[d-2][1]
-      if map[next_a][next_b] == 1:
+      if input_map[next_a][next_b] == 1:
         break
       else:
         a = next_a
