4a0b27cfba26140e898d4e3321e0c2a5695b335d
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-07-19 20:48:11 +0900

    Add files via upload

---
+++ part2/chapter 5/5-1 DFS,BFS 스택.py
@@ -0,0 +1,14 @@
+# 5-1 DFS,BFS 스택
+
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7) #5 2 3 7
+stack.pop()     #5 2 3
+stack.append(1)
+stack.append(4) #5 2 3 1 4
+stack.pop()     #5 2 3 1
+
+print(stack)
---
+++ part2/chapter 5/5-3 DFS,BFS 재귀 함수.py
@@ -0,0 +1,9 @@
+# 5-3 DFS,BFS 재귀 함수
+
+def recursive_function():
+    print("재귀 함수를 호출합니다.")
+    recursive_function()
+    
+recursive_function()
+
+#RecursionError: maximum recursion depth exceeded while calling a Python object
